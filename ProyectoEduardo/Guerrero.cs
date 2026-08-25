dc921a424222a47c114680aef90558a069cd29e9
Luis Angel Reyes Mendoza <zs24022197@estudiantes.uv.mx>
2026-08-24 18:58:38 -0600

    CrearGerrero

--- ProyectoEduardo/Guerrero.cs
+++ ProyectoEduardo/Guerrero.cs
@@ -1,4 +1,13 @@
-class Guerrero
+class Guerrero : Personaje
 {
+    public Guerrero(string nombre) : base(this.nombre)
+    {
 
+    }
+
+    public override void Ataque(Personaje objetivo)
+    {
+        System.Console.WriteLine($"{nombre} ataca con la espada a {objetivo.nombre}");
+        objetivo.RecibirDano(30);
+    }
 }
